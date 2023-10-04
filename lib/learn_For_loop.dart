bcb7edb31114cd63d29eb959a2198f116bfcf41f
Tanuj sharma <tanujsharma482@gmail.com>
2023-10-04 18:03:38 +0530

    learn for loop

---
+++ lib/learn_For_loop.dart
@@ -0,0 +1,26 @@
+void main() {
+  //Loops
+  // For loop
+
+  // for (init; condition; increment/decrement) {
+
+  // }
+
+  // i++ -> i=i+1
+  // i-- -> i=i-1
+  // i+=2 -> i=i+2
+
+  // for (int i = 0; i < 10; i++) {
+  //   String hi = 'Hello World';
+  //   print(hi.substring(i, 11));
+  //   print('Hello World ${i + 1} ');
+  // }
+
+  // for in loop
+
+  String value = 'Hello';
+
+  for (int i = 0; i < value.length; i++) {
+    print(value[i]);
+  }
+}
