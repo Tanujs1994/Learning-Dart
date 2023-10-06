5ab0912aea3c98efcb8f296c108048cfe4ffb242
Tanuj sharma <tanujsharma482@gmail.com>
2023-10-06 13:07:58 +0530

    learn static variables

---
+++ lib/learn_static_variables.dart
@@ -0,0 +1,14 @@
+void main() {
+  // final constants = Constants();
+  print(Constants.greeting);
+  print(Constants.bye);
+}
+
+class Constants {
+  // Constants() {
+  //   print('Constructor called');
+  // }
+ static String greeting = 'Hello, how are you?';
+
+static String bye = 'Bye!';
+}
